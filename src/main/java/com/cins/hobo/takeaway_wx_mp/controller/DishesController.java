862e56763affc443a760d3392363d29482c0aa3c
hobo <1056024860@qq.com>
2021-01-28 14:36:35 +0800

    优化Api接口配置

--- src/main/java/com/cins/hobo/takeaway_wx_mp/controller/DishesController.java
+++ src/main/java/com/cins/hobo/takeaway_wx_mp/controller/DishesController.java
@@ -4,11 +4,10 @@ import com.cins.hobo.takeaway_wx_mp.accessctro.RoleContro;
 import com.cins.hobo.takeaway_wx_mp.enums.RoleEnum;
 import com.cins.hobo.takeaway_wx_mp.form.InsertDishForm;
 import com.cins.hobo.takeaway_wx_mp.form.UpdateDishDetailForm;
+import com.cins.hobo.takeaway_wx_mp.form.UpdateDishTypeForm;
 import com.cins.hobo.takeaway_wx_mp.service.DishesService;
 import com.cins.hobo.takeaway_wx_mp.vo.ResultVO;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -31,33 +30,36 @@ public class DishesController {
     @RoleContro(role = RoleEnum.SUPPER_ADMIN)
     @ApiOperation("添加新的菜品种类")
     @PostMapping(value = "/dish_type")
-    public ResultVO insertDishesType(@ApiParam(value = "菜品种类名称") String dishesTypeName){
+    @ApiImplicitParams({@ApiImplicitParam(name = "dishTypeName", value = "菜品种类名称", required = true,dataTypeClass = String.class)})
+    public ResultVO insertDishesType(String dishesTypeName) {
         return dishesService.insertDishesType(dishesTypeName);
     }
 
     @RoleContro(role = RoleEnum.SUPPER_ADMIN)
     @ApiOperation("更改菜品种类名称")
     @PutMapping(value = "/dish_type")
-    public ResultVO updateDishesType(@ApiParam(value = "菜品种类id") Integer id,@ApiParam(value = "菜品种类名称") String dishTypeName){
-        return dishesService.updateDishesTypeName(id,dishTypeName);
+    public ResultVO updateDishesType(UpdateDishTypeForm form) {
+        return dishesService.updateDishesTypeName(form.getId(), form.getDishTypeName());
     }
 
     @RoleContro(role = RoleEnum.SUPPER_ADMIN)
     @ApiOperation("删除菜品种类")
     @DeleteMapping(value = "/dish_type")
-    public ResultVO deleteDishType(@ApiParam(value = "菜品种类id") Integer id){
+    @ApiImplicitParams({@ApiImplicitParam(name = "id", value = "菜品种类id",required = true,dataTypeClass = Integer.class)})
+    public ResultVO deleteDishType(Integer id) {
         return dishesService.deleteDishesType(id);
     }
 
     @ApiOperation("获取所有的菜品种类")
     @GetMapping(value = "/dish_type")
-    public ResultVO getDishesTypeList(){
+    public ResultVO getDishesTypeList() {
         return dishesService.getDishesTypeList();
     }
 
     @ApiOperation("获取某个菜品种类下的所有菜品")
     @GetMapping(value = "/dish_type/getDishesList")
-    public ResultVO getDishesByDishType(@ApiParam(value = "菜品种类id") Integer typeId){
+    @ApiImplicitParams({@ApiImplicitParam(name = "typeId", value = "菜品种类id", required = true,dataTypeClass = Integer.class)})
+    public ResultVO getDishesByDishType(Integer typeId) {
         return dishesService.getDishesByType(typeId);
     }
 
@@ -65,27 +67,29 @@ public class DishesController {
     @RoleContro(role = RoleEnum.SUPPER_ADMIN)
     @ApiOperation("添加新的菜品")
     @PostMapping("/dish_detail")
-    public ResultVO insertDish(InsertDishForm form, @RequestPart("file") MultipartFile file){
-       return dishesService.insertDish(form,file);
+    public ResultVO insertDish(InsertDishForm form, @RequestPart("file") MultipartFile file) {
+        return dishesService.insertDish(form, file);
     }
 
     @RoleContro(role = RoleEnum.SUPPER_ADMIN)
     @ApiOperation("更新新的菜品")
     @PutMapping(value = "/dish_detail")
-    public ResultVO updateDish(UpdateDishDetailForm dishDetailForm,@RequestPart("file") MultipartFile file){
-        return dishesService.updateDish(dishDetailForm,file);
+    public ResultVO updateDish(UpdateDishDetailForm dishDetailForm, @RequestPart("file") MultipartFile file) {
+        return dishesService.updateDish(dishDetailForm, file);
     }
 
     @RoleContro(role = RoleEnum.SUPPER_ADMIN)
     @ApiOperation("删除菜品")
     @DeleteMapping(value = "/dish_detail")
-    public ResultVO deleteDish(@ApiParam(value = "菜品id") Integer id){
+    @ApiImplicitParams({@ApiImplicitParam(name = "id", value = "菜品id",required = true,dataTypeClass = Integer.class)})
+    public ResultVO deleteDish(Integer id) {
         return dishesService.deleteDish(id);
     }
 
     @ApiOperation("获取菜品详情")
     @GetMapping(value = "/dish_detail")
-    public ResultVO getDishDetail(@ApiParam(value = "菜品id") Integer id){
+    @ApiImplicitParams({@ApiImplicitParam(name = "id", value = "菜品种类id",dataType = "INTEGER", required = true,dataTypeClass = Integer.class)})
+    public ResultVO getDishDetail(Integer id) {
         return dishesService.getDishDetailById(id);
     }
 
---
+++ src/main/java/com/cins/hobo/takeaway_wx_mp/form/UpdateDishTypeForm.java
@@ -0,0 +1,21 @@
+package com.cins.hobo.takeaway_wx_mp.form;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @author : hobo
+ * @className : UpdateDishTypeForm
+ * @date: 2021/1/28
+ * @description: TODO
+ */
+@Data
+public class UpdateDishTypeForm {
+
+    @ApiModelProperty("菜品种类id")
+    private Integer id;
+
+    @ApiModelProperty("菜品种类名称")
+    private String dishTypeName;
+
+}
